8fa79fbff9e5316453bd96afe526eafda4ab3d5c
Roman Shuvaryk <xcomanche@gmail.com>
2019-11-03 15:16:49 -0800

    Fixed goals get

--- routes/goals.js
+++ routes/goals.js
@@ -21,7 +21,7 @@ router.get('/', async (req, res) => {
         return res.status(200).json(goals);
 
     } catch (err) {
-        return res.status(400).json({err: 'USER_FETCH_FAILED', msg: err});
+        return res.status(400).json({err: 'GOALS_FETCH_FAILED', msg: err});
     }
 });
 
